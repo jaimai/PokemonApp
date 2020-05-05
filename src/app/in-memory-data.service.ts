a8e82b0f5f28ec2b485754ed26f62a63c772fed8
Thomas Ferrer <thomas.ferrer@hei.yncrea.fr>
2020-05-05 10:45:19 +0200

    d

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,8 +2,8 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { POKEMONS } from './pokemons/mock-pokemon';
 
 export class InMemoryDataService implements InMemoryDbService {
-    createDb() {
-        let pokemons = POKEMONS;
-        return { pokemons };
-    }
+	createDb() {
+		let pokemons = POKEMONS;
+		return { pokemons };
+	}
 }
--- src/app/pokemons/pokemon.service.ts
+++ src/app/pokemons/pokemon.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './Pokemon';
-import { POKEMONS } from './mock-pokemon';
-
+import 'rxjs/add/operator/map';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
@@ -16,15 +15,16 @@ export class PokemonService
     console.info(log);
   }
 
-  updatePokemon(pokemon: Pokemon): Observable<Pokemon> {
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-type': 'application/json'})
-    };
-    return this.http.put(this.pokemonsUrl, pokemon, httpOptions).pipe(
-      tap(_ => this.log(`updated pokemon id id=${pokemon.id}`)),
-      catchError(this.handleError<any>('updatedPokemon'))
-    );
-  }
+  updatePokemon(pokemon: Pokemon): Observable<any> {
+		const httpOptions = {
+			headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+		};
+
+		return this.http.put(this.pokemonsUrl, pokemon, httpOptions).pipe(
+			tap(_ => this.log(`updated pokemon id=${pokemon.id}`)),
+			catchError(this.handleError<any>('updatePokemon'))
+		);
+	}
 
   private handleError<T>(operation = 'operation' , result?: T){
     return (error: any): Observable<T> => {
@@ -36,22 +36,22 @@ export class PokemonService
   }
 
   // Retourne tous les pokémons
-  getPokemons(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>(this.pokemonsUrl).pipe(
-      tap(_ => this.log(`fetched pokemons`)),
-      catchError(this.handleError(`getPokemons`, []))
-    );
-  }
+	getPokemons(): Observable<Pokemon[]> {
+		return this.http.get<Pokemon[]>(this.pokemonsUrl).pipe(
+			tap(_ => this.log(`fetched pokemons`)),
+			catchError(this.handleError('getPokemons', []))
+		);
+	}
 
   // Retourne le pokémon avec l'identifiant passé en paramètre
-  getPokemon(id: number): Observable<Pokemon>{
-      const url = `${this.pokemonsUrl}/${id}`; //syntaxe ES6
+  getPokemon(id: number): Observable<Pokemon> {
+  		const url = `${this.pokemonsUrl}/${id}`;
 
-      return this.http.get<Pokemon>(url).pipe(
-        tap(_ => this.log(`fetched pokemon id=${id}`)),
-        catchError(this.handleError<Pokemon>(`getPokemon id=${id}`))
-      );
-    }
+  		return this.http.get<Pokemon>(url).pipe(
+  			tap(_ => this.log(`fetched pokemon id=${id}`)),
+  			catchError(this.handleError<Pokemon>(`getPokemon id=${id}`))
+  		);
+  	}
   //Retourne tous les types de pokemons
   getPokemonTypes(): string[] {
         return ['Plante','Feu','Eau','Insecte','Normal','Electrik','Poison','Fée', 'Vol'];
